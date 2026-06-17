343adf31d4d2dc968fd28ad77a95325cf91c70d6
Anwar Radwan <1222275@student.birzeit.edu>
2026-06-18 02:33:39 +0300

    Add Navigation Drawer structure with fragments, custom icons, and updated UI designs

--- app/src/main/java/com/example/a1222275_1220495_courseproject/MainActivity.java
+++ app/src/main/java/com/example/a1222275_1220495_courseproject/MainActivity.java
@@ -1,29 +1,109 @@
- package com.example.a1222275_1220495_courseproject;
+package com.example.a1222275_1220495_courseproject;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-import com.example.a1222275_1220495_courseproject.database.DataBaseHelper;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.a1222275_1220495_courseproject.database.DataBaseHelper;
+import com.example.a1222275_1220495_courseproject.fragments.*;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        // Insert test users for testing purposes (safe to run multiple times)
+
+        // Database setup (retained from original code)
         DataBaseHelper dbHelper = new DataBaseHelper(this);
         dbHelper.insertTestUsers();
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        // Setup Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Setup Navigation Drawer
+        drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        // Load Default Fragment (Home)
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new HomeFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_home);
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment selectedFragment = null;
+        int id = item.getItemId();
+
+        if (id == R.id.nav_home) {
+            selectedFragment = new HomeFragment();
+        } else if (id == R.id.nav_trips) {
+            selectedFragment = new TripsFragment();
+        } else if (id == R.id.nav_reservations) {
+            selectedFragment = new MyReservationsFragment();
+        } else if (id == R.id.nav_favorites) {
+            selectedFragment = new FavoritesFragment();
+        } else if (id == R.id.nav_special) {
+            selectedFragment = new SpecialSectionFragment();
+        } else if (id == R.id.nav_profile) {
+            selectedFragment = new ProfileFragment();
+        } else if (id == R.id.nav_contact) {
+            selectedFragment = new ContactUsFragment();
+        } else if (id == R.id.nav_logout) {
+            handleLogout();
+        }
+
+        if (selectedFragment != null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    selectedFragment).commit();
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    private void handleLogout() {
+        // Clear login session
+        SharedPreferences preferences = getSharedPreferences("LoginPrefs", MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.clear();
+        editor.apply();
+
+        // Navigate back to LoginActivity
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    public void onBackPressed() {
+        // Close drawer if open, otherwise perform standard back action
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
---
+++ app/src/main/java/com/example/a1222275_1220495_courseproject/fragments/ProfileFragment.java
@@ -0,0 +1,20 @@
+package com.example.a1222275_1220495_courseproject.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import com.example.a1222275_1220495_courseproject.R;
+
+public class ProfileFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+}
